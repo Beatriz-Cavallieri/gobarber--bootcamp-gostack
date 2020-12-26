df2dd98447b93f22904f27cde69394b1ee312539
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-12-25 21:45:26 -0300

    Backend: criar classe AppError para erros de rotas no servidor

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -2,6 +2,7 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import AppError from '../errors/AppError';
 
 interface RequestDTO {
     provider_id: string;
@@ -21,7 +22,7 @@ class CreateAppointmentService {
         );
 
         if (hasAppointmentInSameDate) {
-            throw Error('This appointment is already booked');
+            throw new AppError('This appointment is already booked');
         }
 
         const newAppointment = appointmentsRepository.create({
