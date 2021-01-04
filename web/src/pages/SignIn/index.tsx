97a26f684e5a59490c2bd85cc5f57a0cdd182a7c
Beatriz Cavallieri <bfcavallieri@gmail.com>
2021-01-04 18:28:00 -0300

    Web: aplicar aos formularios de sign in e sign up a validaçao de campos com mensagem de erro

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import { Background, Container, Content } from "./styles";
 import logoImg from "../../assets/images/Logo.svg";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 import Button from "../../components/Button";
 import Input from "../../components/Button/Input";
 import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
+import * as Yup from "yup";
+import getValidationErrors from "../../utils/getValidationErrors";
 
 const SignIn: React.FC = () => {
-  function handleSubmit() {}
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("Email obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string().required("Senha obrigatória"),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu login</h1>
           <Input name="email" type="text" placeholder="E-mail" icon={FiMail} />
           <Input
