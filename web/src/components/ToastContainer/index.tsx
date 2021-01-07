653e32f73d90d75f20f61131332ba812529da41c
Beatriz Cavallieri <bfcavallieri@gmail.com>
2021-01-06 21:36:25 -0300

    Web: Adicionar animaçao aos toasts

--- web/src/components/ToastContainer/index.tsx
+++ web/src/components/ToastContainer/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "./styles";
 import { ToastMessage } from "../../hooks/toast";
 import Toast from "./Toast";
+import { useTransition } from "react-spring";
 
 interface ToastContainerProps {
   messages: ToastMessage[];
@@ -10,11 +11,21 @@ interface ToastContainerProps {
 const ToastContainer: React.FunctionComponent<ToastContainerProps> = ({
   messages,
 }) => {
+  // Animação do Toast na entrada e saída
+  const messagesWithTransitions = useTransition(
+    messages,
+    (message) => message.id,
+    {
+      from: { right: "-120%", opacity: 0 },
+      enter: { right: "0%", opacity: 1 },
+      leave: { right: "-120%", opacity: 0 },
+    }
+  );
 
   return (
     <Container>
-      {messages.map((message) => (
-        <Toast key={message.id} message={message}></Toast>
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item}></Toast>
       ))}
     </Container>
   );
